adefaa666db417f19e3fe62cefabb7e73d6faab7
aakash kasturiya <aakashkasturiya@gmail.com>
2021-05-22 18:48:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Logo from './images/Logo.png';
 
-function App() {
+const App = () =>{
+
+  const [data,setData] = useState([]);
+
+  const getCovidData = async()=>{
+    try{
+      const res = await fetch('https://api.covid19india.org/data.json');
+      const actualData = await res.json();
+      setData(actualData.statewise[0]);
+    }catch(err){
+      console.log(err);
+    }
+
+  }
+
+  useEffect(()=>{
+    getCovidData();
+  },[]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+   
+      <Navbar expand="lg" className="Navbar">
+  <Container>
+   <img src={Logo} style={{width:"80px"}}/> <Navbar.Brand href="#">Kasturiya CovidTrackerApp</Navbar.Brand>
+  </Container>
+</Navbar>
+
+      
+<main>
+<section className="heading">
+<p id="blink">&#128308; <span style={{color:"white"}}>Live</span></p>
+  <h2>Covid 19 Tracker</h2>
+</section>
+<div className="parents">
+
+<div className="child">
+
+<h4>Country</h4>
+<p>India</p>
+</div>
+<div className="child">
+<h4>Recovered</h4>
+<p>{data.recovered}</p>
+</div>
+<div className="child">
+<h4>Confirmed</h4>
+<p>{data.confirmed}</p>
+</div>
+<div className="child">
+<h4>Deaths</h4>
+<p>   {data.deaths}</p>
+</div>
+<div className="child">
+<h4>Active</h4>
+<p>  {data.active}</p>
+</div>
+<div className="child">
+<h4>Last Updated</h4>
+<p> {data.lastupdatedtime}</p>
+</div>
+
+</div>
+
+
+</main>
+     
+     
+    <footer className="footer">
+   
+         <p>&#169; 2021 aakashkasturiya@gmail.com</p>
+
+    </footer>
+    
     </div>
   );
 }
